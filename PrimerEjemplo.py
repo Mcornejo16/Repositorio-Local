d164cbde74229815a6d13f70fe031930b54f97da
Mcornejo16 <A01276411@tec.mx>
2025-10-01 16:28:21 -0600

    esto es una prueba

--- PrimerEjemplo.py
+++ PrimerEjemplo.py
@@ -4,3 +4,10 @@ print("Este archivo se estara archivando")
 
 for i in range(10):
     print(i)
+
+
+def suma (a,b):
+    return a + b
+
+print("La suma de los numeros es: ", suma(89,52))
+print("hola de nuevo")
